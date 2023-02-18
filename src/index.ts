777e9326bd683261e2af7fd7ed221df62473488a
saadfrhan <saadfarhan347@gmail.com>
2023-02-19 02:32:05 +0500

    feat: complete project

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,71 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import { StudentManagementSystem } from "./sms.js";
+import inquirer, { QuestionCollection } from "inquirer";
+import { Student, StudentManagementSystem } from "./sms.js";
+import { Actions } from "./types/index.js";
+import { welcome } from "./utils/welcome.js";
 
-async function main() {
-  const sms = new StudentManagementSystem();
-
-  await inquirer.prompt([
+async function main(students?: Student[]) {
+  const sms = new StudentManagementSystem(students);
+  const again = () => main(sms.listStudents());
+  const { action } = await inquirer.prompt([
     {
       type: 'list',
       name: 'action',
       message: 'What would you like to do?',
-      choices: ['Add student', 'Enroll student', 'View balance', 'Pay tuition', 'Show status', 'Exit'],
+      choices: [
+        'Add student',
+        'Enroll student',
+        'Show balance',
+        'List students',
+        'Remove student',
+        'Pay fees',
+        'Show status',
+        'Exit'
+      ],
+      filter: (val) => val.toUpperCase().replace(' ', '_')
     },
-  ]).then(async answers => {
-    switch (answers.action) {
-      case 'Add student':
-        await inquirer.prompt([
-          { type: 'input', name: 'name', message: 'Enter student name:' },
-        ]).then(answers => {
-          sms.addStudent(answers.name);
-          main();
-        });
-        break;
-      case 'Enroll student':
-        sms.enrollStudent();
-        main();
-        break;
-      case 'View balance':
-        sms.viewBalance();
-        main();
-        break;
-      case 'Pay tuition':
-        sms.payTuition();
-        main();
-        break;
-      case 'Show status':
-        sms.showStatus();
-        main();
-        break;
-      case 'Exit':
-        console.log('Exiting...');
-        break;
-    }
-  });
+  ] as QuestionCollection<{ action: Actions }>)
+  switch (action) {
+    case 'ADD_STUDENT':
+      const answers = await inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Enter student name:',
+        validate: (val) => Boolean(val) || 'Please enter student name...'
+      }])
+      sms.addStudent(answers.name);
+      again();
+      break;
+    case 'LIST_STUDENTS':
+      console.table(sms.listStudents());
+      again();
+      break;
+    case 'ENROLL_STUDENT':
+      await sms.enrollStudent();
+      again();
+      break;
+    case 'SHOW_BALANCE':
+      await sms.showBalance();
+      again();
+      break;
+    case 'PAY_FEES':
+      await sms.payFees();
+      again();
+      break;
+    case 'SHOW_STATUS':
+      await sms.showStatus();
+      again();
+      break;
+    case 'REMOVE_STUDENT':
+      await sms.removeStudent();
+      again();
+      break;
+    default:
+      console.log('Exiting...');
+      break;
+  }
 }
 
-// Start the application
-main();
+await welcome();
+await main();
--- src/sms.ts
+++ src/sms.ts
@@ -1,23 +1,22 @@
-import inquirer from "inquirer";
+import inquirer, { QuestionCollection } from "inquirer";
 
-class Student {
-  id: number;
-  name: string;
-  courses?: string[];
-  fees?: number
+const IdQuestion: QuestionCollection<{ id: string }> = {
+  type: 'input',
+  name: 'id',
+  message: 'Enter student ID:',
+  validate: (val) => val.toString().length === 5 || 'Enter 5 digit ID...'
+}
 
+export class Student {
   constructor(
-    name: string,
-    id: number,
-  ) {
-    this.id = id;
-    this.name = name;
-    this.courses = [];
-    this.fees = 0
-  }
+    public name: string,
+    public id: string,
+    public courses: string[] = [],
+    public fees: number = 0
+  ) { }
 
   enroll(course: string) {
-    this.courses?.push(course)
+    this.courses.push(course);
   }
 
   viewFees() {
@@ -25,101 +24,109 @@ class Student {
   }
 
   payFee(fee: number) {
-    if (this.fees) {
-      this.fees -= fee;
-    } else console.log('The Student has no corresponding fees to pay.')
+    if (!this.fees) {
+      console.log('The student has no corresponding fees to pay.');
+      return;
+    }
+    this.fees -= fee;
+    console.log(`Updated student's fee: ${this.fees}`);
   }
 
   showStatus() {
     console.log(`
       Name: ${this.name}
       ID: ${this.id}
-      Courses: ${this.courses}
-      Balance: ${this.fees}
+      Courses: ${this.courses || 'None'}
+      Pending fees: ${this.fees}
     `);
   }
-
 }
 
-
-// Student management system class
 export class StudentManagementSystem {
-  students: Student[];
-  currentID: number;
+  private students: Student[];
 
-  constructor() {
-    this.students = [];
-    this.currentID = 10000;
+  constructor(students?: Student[]) {
+    this.students = students ?? [];
   }
 
   // Add student method
   addStudent(name: string) {
-    const student = new Student(name, this.currentID);
-    this.currentID++;
+    const student = new Student(
+      name,
+      (Math.floor(Math.random() * 99999)).toString()
+    );
     this.students.push(student);
     console.log(`Student ${name} added. ID: ${student.id}`);
   }
 
-  // Enroll student method
-  enrollStudent() {
-    inquirer.prompt([
-      { type: 'input', name: 'id', message: 'Enter student ID:' },
-      { type: 'input', name: 'course', message: 'Enter course name:' },
-    ]).then(answers => {
-      const student = this.students.find(s => s.id === parseInt(answers.id));
-      if (student) {
-        student.enroll(answers.course);
-        console.log(`Student ${student.name} enrolled in course ${answers.course}.`);
-      } else {
-        console.log(`Student with ID ${answers.id} not found.`);
-      }
-    });
+  async enrollStudent() {
+    const { id, course } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'course',
+        message: 'Enter course name:',
+        validate: (val) => Boolean(val) || 'Please enter course name...',
+      },
+      ...[IdQuestion]
+    ] as QuestionCollection<{ id: string, course: string }>);
+    const student = this.students.find(s => s.id === id);
+    if (student) {
+      student.enroll(course);
+      student.fees += 500
+      console.log(`Student ${student.name} enrolled in course ${course}.`);
+    } else {
+      console.log(`Student with ID ${id} not found.`);
+    }
+  }
+
+  async removeStudent() {
+    const { id } = await inquirer.prompt([IdQuestion]);
+    const idx = this.students.findIndex(s => s.id === id);
+    this.students.splice(idx, 1);
+    console.log(`Updated student list:`)
+    console.log(JSON.stringify(this.students, null, 2));
   }
 
   listStudents() {
     return this.students;
   }
 
-  // View balance method
-  viewBalance() {
-    inquirer.prompt([
-      { type: 'input', name: 'id', message: 'Enter student ID:' },
-    ]).then(answers => {
-      const student = this.students.find(s => s.id === parseInt(answers.id));
-      if (student) {
-        student.viewFees();
-      } else {
-        console.log(`Student with ID ${answers.id} not found.`);
-      }
-    });
+  async showBalance() {
+    const { id } = await inquirer.prompt([IdQuestion]);
+    const student = this.students.find(s => s.id === id);
+    if (student) {
+      student.viewFees();
+    } else {
+      console.log(`Student with ID ${id} not found.`);
+    }
   }
 
-  // Pay tuition method
-  payTuition() {
-    inquirer.prompt([
-      { type: 'input', name: 'id', message: 'Enter student ID:' },
-      { type: 'input', name: 'amount', message: 'Enter payment amount:' },
-    ]).then(answers => {
-      const student = this.students.find(s => s.id === parseInt(answers.id));
-      if (student) {
-        student.payFee(parseInt(answers.amount));
-      } else {
-        console.log(`Student with ID ${answers.id} not found.`);
-      }
-    });
+  async payFees() {
+    const { id, amount } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'amount',
+        message: 'Enter payment amount:',
+        validate: (val) => !(isNaN(val)) || 'Enter amount in numbers...',
+      },
+      ...[IdQuestion]
+    ] as QuestionCollection<{ id: string; amount: number }>)
+    const student = this.students.find(s => s.id === id);
+    if (student) {
+      student.payFee(Number(amount));
+      console.log(`Updated student's fee: ${student.fees}`)
+    } else {
+      console.log(`Student with ID ${id} not found.`);
+    }
   }
 
-  // Show status method
-  showStatus() {
-    inquirer.prompt([
-      { type: 'input', name: 'id', message: 'Enter student ID:' },
-    ]).then(answers => {
-      const student = this.students.find(s => s.id === parseInt(answers.id));
-      if (student) {
-        student.showStatus();
-      } else {
-        console.log(`Student with ID ${answers.id} not found.`);
-      }
-    });
+  async showStatus() {
+    const { id } = await inquirer.prompt([IdQuestion]);
+    const student = this.students.find(s => s.id === id);
+    if (student) {
+      student.showStatus();
+    } else {
+      console.log(`Student with ID ${id} not found.`);
+    }
   }
 }
